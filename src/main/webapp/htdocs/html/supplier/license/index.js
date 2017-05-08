48c1efa9892500fae5389ab2534e086b94e41d74
yadda <silenceisok@163.com>
2017-05-08 06:54:56 +0000

    zTree模块化封装

--- src/main/webapp/htdocs/html/supplier/license/index.js
+++ src/main/webapp/htdocs/html/supplier/license/index.js
@@ -1,3 +1,13 @@
 /**
  * Created by yadda on 2017/5/8.
  */
+;(function () {
+
+    var $ = require('$');
+    var SMS = require('SMS');
+
+    var Tree = require('Tree');
+
+    tree.render();
+
+})();
