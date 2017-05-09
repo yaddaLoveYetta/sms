0c303c9616a79e8c0668ba27c469a620fdf8d4bb
yadda <silenceisok@163.com>
2017-05-09 01:49:43 +0000

    供应商资质管理

--- src/main/webapp/htdocs/html/supplier/license/modules/Tree.js
+++ src/main/webapp/htdocs/html/supplier/license/modules/Tree.js
@@ -11,35 +11,86 @@ define('Tree', function (require, module, exports) {
     var emitter = MiniQuery.Event.create();
     var container = document.getElementById('tree');
 
-    function load(fn) {
+    //默认配置
+    var defaults = {
+        pageSize: 10,
+        pageNo: 1,
+    };
 
-        var treeNodes = [
-            {"id": 1, "pId": 0, "name": "test1"},
-            {"id": 11, "pId": 1, "name": "test11"},
-            {"id": 12, "pId": 1, "name": "test12"},
-            {"id": 111, "pId": 11, "name": "test111"},
-        ];
+    function load(config, fn) {
 
-        fn && fn(treeNodes);
+
+        var api = new API('template/getItems');
+
+        var params = {
+            'classId': config.classId,
+            'pageNo': config.pageNo,
+            'pageSize': config.pageSize,
+            'condition': conditions.length > 0 ? conditions : '',
+        };
+
+        api.post(params);
+
+        api.on({
+            'success': function (data, json) {
+                fn && fn(data, json);
+            },
+
+            'fail': function (code, msg, json) {
+                var s = $.String.format('{0} (错误码: {1})', msg, code);
+                SMS.Tips.error(s);
+            },
+
+            'error': function () {
+                SMS.Tips.error('网络繁忙，请稍候再试');
+            }
+        });
+
+
+        /*        var treeNodes = [
+         {"id": 1, "pId": 0, "name": "test1"},
+         {"id": 11, "pId": 1, "name": "test11"},
+         {"id": 12, "pId": 1, "name": "test12"},
+         {"id": 111, "pId": 11, "name": "test111"},
+         ];
+
+         fn && fn(treeNodes);*/
     };
 
     function render() {
 
+        load({
+            classId: 1005,
+            pageNo: 1,
+            pageSize: defaults.pageSize,
+            conditions: [],
+        }, function (data, pageSize) {
 
-        load(function (data) {
+            Pager.render({
+                size: pageSize,
+                total: data.count,
+                change: function (no) {
+                    List.render({
+                        classId: classId,
+                        pageNo: no,
+                        pageSize: defaults.pageSize,
+                        conditions: [],
+                    });
+                }
+            });
 
             SMS.use('ZTree', function (zTree) {
 
                 var tree = new zTree({
                     selector: '#tree',
-                    data: data,
+                    data: data.list,
                 });
 
                 tree.on({
 
                     onClick: function (event, treeId, treeNode) {
                         console.log(treeNode.id + ", " + treeNode.name);
-                        emitter.fire("onClick" , [treeNode]);
+                        emitter.fire("onClick", [treeNode]);
                     },
                 });
 
