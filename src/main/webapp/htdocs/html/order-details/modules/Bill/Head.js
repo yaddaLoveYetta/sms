1e446a4bd7fbb24b250c5836532f33e6ab12bdab
yadda <silenceisok@163.com>
2017-05-15 04:02:40 +0000

    采购订单明细页面

--- src/main/webapp/htdocs/html/order-details/modules/Bill/Head.js
+++ src/main/webapp/htdocs/html/order-details/modules/Bill/Head.js
@@ -7,9 +7,55 @@ define('Bill/Head', function (require, module, exports) {
 
     var data;
 
+    var div = document.getElementById("dd-head");
+    var samples = require("/Samples")(div);
+
+
     function render(template, data) {
         data = data;
-        console.log(data);
+
+
+        div.innerHTML = $.String.format(samples["table"], {
+            checkbox: data.checkbox ? samples["th.checkbox"] : "",
+            ths: $.Array.keep(headItems, function (field, index) {
+                return $.String.format(samples["th"], {
+                    index: index,
+                    th: field.text,
+                    width: field.width
+                });
+            }).join(""),
+            trs: $.Array.keep(bodyItems, function (item, no) {
+                // 行
+                return $.String.format(samples["tr"], {
+                    index: no,
+                    "disabled-class": item.disabled ? "disabled" : "",
+                    checkbox: data.checkbox ? $.String.format(samples["td.checkbox"], {
+                        index: no
+                    }) : "",
+                    tds: $.Array.keep(item.items, function (item, index) {
+                        // 列
+                        var field = headItems[index];
+                        // 当前列的表头信息
+                        return $.String.format(samples["td"], {
+                            index: index,
+                            key: field.key,
+                            td: field.isEntry ? getHtml("entry", item.value) : getHtml(field.type, item.value),
+                            "number-class": field.key == "number" ? "number" : ""
+                        });
+                    }).join("")
+                });
+            }).join(""),
+            emptys: data.checkbox ? samples["emptytd"] : "",
+            tdtotals: $.Array.keep(headItems, function (field, index) {
+                return $.String.format(samples["tdtotal"], {
+                    index: index,
+                    key: field.key,
+                    needTotal: field.isCount == "1",//(field.type == 1 && field.lookupType == 0),
+                    width: field.width
+                });
+            }).join("")
+        });
+
     }
 
     return {
