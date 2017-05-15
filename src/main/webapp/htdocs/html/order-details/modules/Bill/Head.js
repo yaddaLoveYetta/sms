75e2434618b392e475723271df516c811cef36d7
yadda <silenceisok@163.com>
2017-05-15 09:11:41 +0000

    采购订单明细页面

--- src/main/webapp/htdocs/html/order-details/modules/Bill/Head.js
+++ src/main/webapp/htdocs/html/order-details/modules/Bill/Head.js
@@ -12,54 +12,10 @@ define('Bill/Head', function (require, module, exports) {
 
 
         function render(template, data) {
-            data = data;
 
-            var i = 0;
             var fields = template['0'];
 
             fields = $.Array.group(fields, 4); // 分组，一行填充4个字段
-
-            var innerHtml = '';
-            var sample;
-
-            var length = $.Object.getLength(fields);
-
-            /*        for (var key in fields) {
-
-             if (i / 4 === 0) {
-             sample = samples["rows"]
-             }else {
-             sample = samples["row"]
-             }
-             var field = fields[key];
-
-             innerHtml += $.String.format(sample, {});
-
-             i++;
-             }*/
-
-            /*            div.innerHTML = $.String.format(samples["rows"], {
-
-             item: $.Array.keep(fields, function (group, no) {
-
-
-             return $.Array.keep(group, function (field, no) {
-
-             if (!field.visible) {
-             return '';
-             }
-             return $.String.format(samples["row"], {
-             name: group.name,
-             value: data[group.key],
-             })
-
-             }).join("");
-
-
-             }).join(""),
-
-             });*/
-
 
             div.innerHTML = $.Array.keep(fields, function (group, no) {
 
@@ -67,12 +23,21 @@ define('Bill/Head', function (require, module, exports) {
 
                     item: $.Array.keep(group, function (field, no) {
 
+                        var key = field.key;
+
                         if (!field.visible) {
                             return '';
                         }
+
+                        if (field.lookupType > 0 && field.lookupType < 3) { // lookupType
+                            // 不为 0时，说明是引用类型
+                            key = key + '_DspName';
+                            // 此时要显示的字段为 key + '_DspName'
+                        }
+
                         return $.String.format(samples["row"], {
                             name: field.text,
-                            value: getHtml(field.type, data[field.key]),
+                            value: getHtml(field.type, data[key]),
                         })
 
                     }).join(""),
