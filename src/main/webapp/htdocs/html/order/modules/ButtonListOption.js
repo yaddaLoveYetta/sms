59407388e5bfa98d3878d9443279425d742c2653
yadda <silenceisok@163.com>
2017-05-26 05:53:14 +0000

    发货单列表-详情

--- src/main/webapp/htdocs/html/order/modules/ButtonListOption.js
+++ src/main/webapp/htdocs/html/order/modules/ButtonListOption.js
@@ -8,48 +8,51 @@ define('ButtonListOption', function (require, module, exports) {
 
     function get(classId) {
 
-        if (classId == 2019) {
-            // 采购订单
-            config = {
-                'items': [
-                    {
-                        text: '接单',
-                        name: 'tick',
-                    },
-                    {
-                        text: '刷新',
-                        name: 'refresh',
-                    },
-                    {
-                        text: '详情',
-                        name: 'detail',
-                    },
-                    {
-                        text: '生成发货单',
-                        name: 'deliver',
-                    }
-                ]
-            };
-        } else if (classId == 2020) {
-            // 发货单
-            config = {
-                'items': [
-                    {
-                        text: '刷新',
-                        name: 'refresh',
-                    },
-                    {
-                        text: '详情',
-                        name: 'detail',
-                    },
-                ]
-            };
-        } else {
+        switch (classId) {
 
-            config = {
-                'items': []
-            };
-
+            case 2019:
+                // 采购订单
+                config = {
+                    'items': [
+                        {
+                            text: '接单',
+                            name: 'tick',
+                        },
+                        {
+                            text: '刷新',
+                            name: 'refresh',
+                        },
+                        {
+                            text: '详情',
+                            name: 'detail',
+                        },
+                        {
+                            text: '生成发货单',
+                            name: 'deliver',
+                        }
+                    ]
+                };
+                break;
+            case 2020:
+                // 发货单
+                config = {
+                    'items': [
+                        {
+                            text: '刷新',
+                            name: 'refresh',
+                        },
+                        {
+                            text: '详情',
+                            name: 'detail',
+                        },
+                    ]
+                };
+                break;
+            default:
+                config = {
+                    'items': []
+                };
+                break;
         }
 
         return config;
