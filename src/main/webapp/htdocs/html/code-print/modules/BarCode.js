91e6f753b76dd2d15e311bcdaa396d45a21f8fec
yadda <silenceisok@163.com>
2017-06-01 09:25:05 +0000

    唯一码打印

--- src/main/webapp/htdocs/html/code-print/modules/BarCode.js
+++ src/main/webapp/htdocs/html/code-print/modules/BarCode.js
@@ -11,10 +11,30 @@ define('BarCode', function (require, module, exports) {
     var target = $('#code');
     var item = $("<div class='bcTarget'></div>");
 
+    var _default = {
+        barWidth: 1,
+        barHeight: 50,
+        moduleSize: 5,
+        showHRI: false,
+        bgColor: '#FFFFFF',
+        color: '#000000',
+        fontSize: 10,
+        output: 'css',
+    };
+
     function render(code) {
 
         for (var i = 0; i < code.length; i++) {
-            item.clone().appendTo(target).barcode("1234567890128", "ean13");
+            item.clone().appendTo(target).barcode("1234567890128", "ean13", {
+                barWidth: 1,
+                barHeight: 50,
+                moduleSize: 5,
+                showHRI: true,
+                bgColor: '#FFFFFF',
+                color: '#000000',
+                fontSize: 12,
+                output: 'css',
+            });
         }
     }
 
