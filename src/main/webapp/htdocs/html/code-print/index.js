3acd2253954aa3e549a2f231bbc41e104137d4f7
yadda <silenceisok@163.com>
2017-06-01 08:04:40 +0000

    唯一码打印

--- src/main/webapp/htdocs/html/code-print/index.js
+++ src/main/webapp/htdocs/html/code-print/index.js
@@ -1,3 +1,43 @@
 /**
  * Created by yadda on 2017/6/1.
  */
+;(function () {
+
+
+    var $ = require('$');
+    var MiniQuery = require('MiniQuery');
+    var SMS = require('SMS');
+    var ButtonList = require('ButtonList');
+    var Iframe = SMS.require('Iframe');
+
+    var code = [];// 条形码字符内容
+
+    //检查登录
+    if (!SMS.Login.check(true)) {
+        return;
+    }
+
+    var dialog = Iframe.getDialog();
+
+    if (dialog) {
+
+        var data = dialog.getData();
+
+        code = data.code;
+
+    } else {
+        return;
+    }
+
+    // 支持二级事件，二级事件对应 item 中的 name
+    ButtonList.on('click', {
+        'optPrint': function () {
+            alert('optPrint');
+        },
+        'optRefresh': function () {
+            Edit.refresh(formClassId);
+        }
+    });
+
+
+})();
