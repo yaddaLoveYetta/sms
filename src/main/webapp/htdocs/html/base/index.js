2da63f5834a3481e42ab3964f6c992d79fadfd5a
yadda <silenceisok@163.com>
2017-06-05 03:37:05 +0000

    供应商资质类别-物料证件类别-发送功能

--- src/main/webapp/htdocs/html/base/index.js
+++ src/main/webapp/htdocs/html/base/index.js
@@ -201,10 +201,10 @@
                 SMS.Tips.error('请选择要操作的项');
                 return;
             }
-            if (list.length > 1) {
+/*            if (list.length > 1) {
                 SMS.Tips.error('一次只能对一条记录进行操作');
                 return;
-            }
+            }*/
             if (list[0].data.review === 0) {
                 SMS.Tips.error('该记录未审核，不可发送');
                 return;
