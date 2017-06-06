2fcdf956e1f85207d02ae1d04336f173ab411b3c
yadda <silenceisok@163.com>
2017-06-06 08:33:18 +0000

    个体码打印-增加条码格式选择

--- src/main/webapp/htdocs/html/code-print/modules/BarCode.js
+++ src/main/webapp/htdocs/html/code-print/modules/BarCode.js
@@ -14,6 +14,9 @@ define('BarCode', function (require, module, exports) {
     var div = document.getElementById("code");
     var samples = require("Samples")(div);
 
+    var codeTypeDiv=document.getElementById('div-code-style');
+
+
     var _default = {
         barWidth: 1,
         barHeight: 50,
@@ -27,33 +30,6 @@ define('BarCode', function (require, module, exports) {
 
     function render(code) {
 
-        /*        for (var i = 0; i < code.length; i++) {
-         codeItem.clone().appendTo(target).barcode("1234567890128", "ean13", {
-         barWidth: 1,
-         barHeight: 50,
-         moduleSize: 5,
-         showHRI: true,
-         bgColor: '#FFFFFF',
-         color: '#000000',
-         fontSize: 12,
-         output: 'css',
-         });
-         }*/
-
-        /**
-         code: codeItem.clone().barcode(item.text, "ean13", {
-                    addQuietZone: false,      //是否添加空白区（内边距）
-                    bgColor: "#FFFFFF",  //条码背景颜色
-                    color: "#000000",    //条码颜色
-                    fontSize: 12,   //条码字体大小
-                    marginHRI: 5,  //条码字体距条码中心的高度
-                    barWidth: "1",  //单条条码宽度
-                    barHeight: "60",  //单体条码高度
-                    moduleSize: 5,   //条码大小
-                    output: 'css', //渲染方式 css/bmp/svg/canvas
-                })
-         */
-
         div.innerHTML = $.Array.keep(code, function (item, index) {
 
             return $.String.format(samples["codes"], {
@@ -66,6 +42,7 @@ define('BarCode', function (require, module, exports) {
 
         }).join('');
 
+        var type=$('code');
 
         SMS.use('BarCode', function (BarCode) {
 
