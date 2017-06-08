6aa8352341d10f7b0197124765cbf45742094730
yadda <silenceisok@163.com>
2017-06-08 09:09:39 +0000

    uploadify上传附件

--- src/main/webapp/htdocs/html/supplier/upload/modules/Upload.js
+++ src/main/webapp/htdocs/html/supplier/upload/modules/Upload.js
@@ -20,7 +20,7 @@ define('Upload', function (require, module, exports) {
     //销毁Uploadify实例并将文件上传按钮恢复到原始状态
     function refrshUploadify() {
         btn.uploadify('destroy');
-        render();
+        //render();
     }
 
     function render(opts) {
