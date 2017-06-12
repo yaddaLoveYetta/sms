06c157e4765ef319c4844c1e629302261bc0e918
yadda <silenceisok@163.com>
2017-06-12 08:19:40 +0000

    序时簿控件

--- src/main/webapp/htdocs/html/base/modules/List/Samples.js
+++ src/main/webapp/htdocs/html/base/modules/List/Samples.js
@@ -1,9 +1,7 @@
-﻿
-
-/**
-* List/Samples 模块
-* 
-*/
+﻿/**
+ * List/Samples 模块
+ *
+ */
 define('List/Samples', function (require, module, exports) {
 
     var $ = require('$');
@@ -57,6 +55,12 @@ define('List/Samples', function (require, module, exports) {
                 //fn: trim,
             },
             {
+                name: 'td.value',
+                begin:'#--td.value.begin--#',
+                end:' #--td.value.end--#',
+                outer:'{td}'
+            },
+            {
                 name: 'item.a',
                 begin: '!--item.a.begin--!',
                 end: '!--item.a.end--!',
@@ -64,20 +68,20 @@ define('List/Samples', function (require, module, exports) {
                 //fn: trim,
             },
             {
-                name:'item.table',
-                begin:'!--item.table.begin--!',
-                end:'!--item.table.end--!',
+                name: 'item.table',
+                begin: '!--item.table.begin--!',
+                end: '!--item.table.end--!',
                 outer: '{item_table}',
             },
             {
-                name:'item.table.tr',
-                begin:'!--item.table.tr.begin--!',
-                end:'!--item.table.tr.end--!',
+                name: 'item.table.tr',
+                begin: '!--item.table.tr.begin--!',
+                end: '!--item.table.tr.end--!',
                 outer: '{item_table_tr}',
-            },{
-                name:'item.table.tr.td',
-                begin:' !--item.table.tr.td.begin--!',
-                end:'!--item.table.tr.td.end--!',
+            }, {
+                name: 'item.table.tr.td',
+                begin: ' !--item.table.tr.td.begin--!',
+                end: '!--item.table.tr.td.end--!',
                 outer: '{item_table_tr_td}',
             },
             {
@@ -101,12 +105,11 @@ define('List/Samples', function (require, module, exports) {
                 outer: '{emptys}'
                 //fn: trim,
             }
-            
+
         ]);
 
         return samples;
     }
-
 
 
     return get;
