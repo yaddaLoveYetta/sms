cb719a1eb1f3b09688e0197a70c82d0cad8174c1
yadda <silenceisok@163.com>
2017-07-21 08:52:43 +0000

    明细菜单位置调整

--- src/main/webapp/htdocs/modules/main/Menus.js
+++ src/main/webapp/htdocs/modules/main/Menus.js
@@ -129,14 +129,14 @@ define('Menus', function (require, module, exports) {
         $(div).toggleClass('menus-' + index, !isFixed);
 
         // 重算菜单高度
-        div.offsetHeight = ($(document.body).height() - 85 - div.offsetHeight) < 0 ? $(document.body).height() - 85 : div.offsetHeight;
+        var h = ($(document.body).height() - 85 - div.offsetHeight) < 0 ? $(document.body).height() - 85 : div.offsetHeight;
         // 重算菜单位置
         var t = $(document.body).height() - 85 - div.offsetTop - div.offsetHeight;
 
         $(div).css('top', '');
 
         if (t < 0) {
-
+            $(div).css('height', h);
             $(div).css('top', 85);// 顶部fixed预留85px
         }
 
