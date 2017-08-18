2a2f2895ff9579de6f353738f14f8e2c14008ad4
yadda <silenceisok@163.com>
2017-08-18 01:41:27 +0000

    供应商资质/物料证件 增加导出功能

--- src/test/java/com/kingdee/eas/hrp/sms/common/Common.java
+++ src/test/java/com/kingdee/eas/hrp/sms/common/Common.java
@@ -1,5 +1,7 @@
 package com.kingdee.eas.hrp.sms.common;
 
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.junit.Test;
 
 import com.google.common.base.Charsets;
@@ -28,5 +30,12 @@ public class Common {
 		String utf_82 = new String(ios8859_1.getBytes(Charsets.ISO_8859_1), Charsets.UTF_8);
 		System.out.println(utf_82);
 
+	}
+	@Test
+	public void out2Excel(){
+		// 声明工作簿
+		HSSFWorkbook wb= new HSSFWorkbook();
+		// 声明工作表
+		HSSFSheet sheet= wb.createSheet("供应商资质");
 	}
 }
--- src/test/java/com/kingdee/eas/hrp/sms/controller/FileUploadControllerTest.java
+++ src/test/java/com/kingdee/eas/hrp/sms/controller/FileUploadControllerTest.java
@@ -3,10 +3,15 @@
  */
 package com.kingdee.eas.hrp.sms.controller;
 
+import java.util.Map;
+
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import com.kingdee.eas.hrp.sms.service.api.ITemplateService;
+import com.kingdee.eas.hrp.sms.util.Environ;
 import com.kingdee.eas.hrp.sms.util.http.HttpParam;
 import com.kingdee.eas.hrp.sms.util.http.HttpUtil;
 
@@ -16,8 +21,6 @@ import com.kingdee.eas.hrp.sms.util.http.HttpUtil;
  */
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FileUploadControllerTest extends BaseControllerTest {
-
-
 
 	@Test
 	public void delete() {
@@ -29,5 +32,19 @@ public class FileUploadControllerTest extends BaseControllerTest {
 
 		String ret = HttpUtil.sendGet(BASE_URL + "file/delete", param);
 		System.out.println(ret);
+	}
+
+	@Test
+	public void export() {
+
+		HttpParam param = HttpParam.init();
+		param.setCookieParams(cookie);
+		param.addCommon("classId", "3010");
+		param.addCommon("condition", "");
+
+		String ret = HttpUtil.sendGet(BASE_URL + "file/export", param);
+		System.out.println(ret);
+		// ExcelUtil.exportExcelX("", formTemplate, items, false);
+		
 	}
 }
