fc3c123ca788e720dfd1f3e7401f3f23da55d160
yadda <silenceisok@163.com>
2017-08-30 02:10:02 +0000

    物料证件新增时增加一证件对多物料功能

--- src/main/webapp/htdocs/html/supplier/batchAdd/index.js
+++ src/main/webapp/htdocs/html/supplier/batchAdd/index.js
@@ -19,10 +19,6 @@
 
     var blConfig = {
         items: [{
-            text: '新增',
-            name: 'optNew',
-            icon: '../../../css/main/img/add.png',
-        }, {
             text: '保存',
             name: 'optSave',
             icon: '../../../css/main/img/save.png',
@@ -44,21 +40,9 @@
     // 基础资料类别ID
     // 支持二级事件，二级事件对应 item 中的 name
     ButtonList.on('click', {
-        'optNew': function () {
-            Edit.clear();
-        },
         'optSave': function () {
             Edit.save();
         },
-        'optRefresh': function () {
-            Edit.refresh(formClassId);
-        },
-        'optDisable': function () {
-            Edit.forbid(formClassId, 1);
-        },
-        'optUndisable': function () {
-            Edit.forbid(formClassId, 0);
-        }
     });
 
     Edit.on({
