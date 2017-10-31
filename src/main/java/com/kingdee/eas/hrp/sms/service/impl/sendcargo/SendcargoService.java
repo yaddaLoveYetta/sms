7c944b64b19a18e804b4c77648d84ed891a6ddc1
yadda <silenceisok@163.com>
2017-10-31 11:45:47 +0800

    代码修正

--- src/main/java/com/kingdee/eas/hrp/sms/service/impl/sendcargo/SendcargoService.java
+++ src/main/java/com/kingdee/eas/hrp/sms/service/impl/sendcargo/SendcargoService.java
@@ -27,115 +27,117 @@ import com.kingdee.eas.hrp.sms.util.Environ;
 @Service
 public class SendcargoService extends BaseService implements ISendcargoService {
 
-	@Resource
-	IWebService IWebService;
+    @Resource
+    IWebService IWebService;
 
-	@Override
-	public List<Map<String, Object>> getCode(String items) {
+    @Override
+    public List<Map<String, Object>> getCode(String items) {
 
-		SendcargoDaoMapper sendcargoDaoMapper = sqlSession.getMapper(SendcargoDaoMapper.class);
-		List<String> list = new ArrayList<String>();
-		String[] split = items.split("\\,");
-		for (int i = 0; i < split.length; i++) {
-			String id = split[i];
-			list.add(id);
-		}
+        SendcargoDaoMapper sendcargoDaoMapper = sqlSession.getMapper(SendcargoDaoMapper.class);
+        List<String> list = new ArrayList<String>();
+        String[] split = items.split("\\,");
+        for (int i = 0; i < split.length; i++) {
+            String id = split[i];
+            list.add(id);
+        }
 
-		List<Map<String, Object>> map = sendcargoDaoMapper.selectInvoiceById(list);
-		if (map == null || map.size() == 0) {
-			throw new BusinessLogicRunTimeException("发货单无个体码数据");
-		}
-		return map;
+        List<Map<String, Object>> map = sendcargoDaoMapper.selectInvoiceById(list);
+        if (map == null || map.size() == 0) {
+            throw new BusinessLogicRunTimeException("发货单无个体码数据");
+        }
+        return map;
 
-	}
+    }
 
-	@Override
-	@Transactional
-	public void sendCargoHrp(String items) {
+    @Override
+    @Transactional
+    public void sendCargoHrp(String items) {
 
-		SendcargoDaoMapper sendcargoDaoMapper = sqlSession.getMapper(SendcargoDaoMapper.class);
+        SendcargoDaoMapper sendcargoDaoMapper = sqlSession.getMapper(SendcargoDaoMapper.class);
 
-		JSONArray lists = new JSONArray();
+        JSONArray lists = new JSONArray();
 
-		String[] split = items.split("\\,");
+        String[] split = items.split("\\,");
 
-		for (int i = 0; i < split.length; i++) {
-			List<Map<String, Object>> map = sendcargoDaoMapper.selectSendCargoANDEntry(split[i]);
-			JSONObject entry = new JSONObject();
+        for (int i = 0; i < split.length; i++) {
 
-			JSONObject json = new JSONObject();
-			ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+            List<Map<String, Object>> item = sendcargoDaoMapper.selectSendCargoANDEntry(split[i]);
+            JSONObject entry = new JSONObject();
 
-			if (null != map) {
-				for (int j = 0; j < map.size(); j++) {
+            JSONObject json = new JSONObject();
+            ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
-					JSONObject entrys = new JSONObject();
+            if (null != item) {
 
-					Map<String, Object> sendCargo = map.get(j);
-					if (sendCargo.get("type") != null && !sendCargo.get("type").equals("")) {
-						if (Integer.parseInt(sendCargo.get("type").toString()) == 1) {
-							throw new BusinessLogicRunTimeException("已发送到医院的发货单不能再次发送");
-						}
-					}
+                for (int j = 0; j < item.size(); j++) {
 
-					if (j == 0) {
-						// 表头
-						json.put("id", sendCargo.get("id"));
-						json.put("supplier", sendCargo.get("supplier"));
-						json.put("number", sendCargo.get("number"));
-					}
-					entrys.put("entryId", sendCargo.get("entryId"));
-					entrys.put("seq", sendCargo.get("seq"));
-					entrys.put("purOrder", sendCargo.get("orderId"));
-					entrys.put("purOrderEntry", sendCargo.get("orderEntryId"));
-					entrys.put("material", sendCargo.get("material"));
-					entrys.put("lot", sendCargo.get("lot"));
-					entrys.put("plotNum", sendCargo.get("dyBatchNum"));
-					entrys.put("innerCode", sendCargo.get("code"));
-					entrys.put("dyPrice", sendCargo.get("price"));
-					entrys.put("unit", sendCargo.get("unit"));
-					entrys.put("qty", sendCargo.get("actualQty"));
-					entrys.put("dyProDate", sendCargo.get("dyProDate"));
-					entrys.put("dyProduct", sendCargo.get("dyManufacturer"));
-					entrys.put("dyRegistNum", sendCargo.get("registrationNo"));
-					entrys.put("dyEffDate", sendCargo.get("effectiveDate"));
-					entrys.put("dyamount", sendCargo.get("amount"));
-					entrys.put("remark", "来自供应商平台发货单[" + sendCargo.get("number") + "]行号[" + sendCargo.get("seq"));
-					list.add(entrys);
-				}
+                    JSONObject entrys = new JSONObject();
 
-				entry.put("1", list);
-				json.put("entry", entry);
-				lists.add(json);
-			}
-		}
+                    Map<String, Object> sendCargo = item.get(j);
+                    if (sendCargo.get("type") != null && !sendCargo.get("type").equals("")) {
+                        if (Integer.parseInt(sendCargo.get("type").toString()) == 1) {
+                            throw new BusinessLogicRunTimeException("已发送到医院的发货单不能再次发送");
+                        }
+                    }
 
-		String response = IWebService.webService(lists.toString(), "sms2hrpSendCargo");
-		JSONObject rps = JSONObject.parseObject(response);
-		if (rps == null || rps.equals("")) {
-			throw new BusinessLogicRunTimeException("网络错误!请稍后再试");
-		}
+                    if (j == 0) {
+                        // 表头
+                        json.put("id", sendCargo.get("id"));
+                        json.put("supplier", sendCargo.get("supplier"));
+                        json.put("number", sendCargo.get("number"));
+                    }
+                    entrys.put("entryId", sendCargo.get("entryId"));
+                    entrys.put("seq", sendCargo.get("seq"));
+                    entrys.put("purOrder", sendCargo.get("orderId"));
+                    entrys.put("purOrderEntry", sendCargo.get("orderEntryId"));
+                    entrys.put("material", sendCargo.get("material"));
+                    entrys.put("lot", sendCargo.get("lot"));
+                    entrys.put("plotNum", sendCargo.get("dyBatchNum"));
+                    entrys.put("innerCode", sendCargo.get("code"));
+                    entrys.put("dyPrice", sendCargo.get("price"));
+                    entrys.put("unit", sendCargo.get("unit"));
+                    entrys.put("qty", sendCargo.get("actualQty"));
+                    entrys.put("dyProDate", sendCargo.get("dyProDate"));
+                    entrys.put("dyProduct", sendCargo.get("dyManufacturer"));
+                    entrys.put("dyRegistNum", sendCargo.get("registrationNo"));
+                    entrys.put("dyEffDate", sendCargo.get("effectiveDate"));
+                    entrys.put("dyamount", sendCargo.get("amount"));
+                    entrys.put("remark", "来自供应商平台发货单[" + sendCargo.get("number") + "]行号[" + sendCargo.get("seq"));
+                    list.add(entrys);
+                }
 
-		if (rps.get("code").equals("200")) {
-			PlatformTransactionManager txManager = Environ.getBean(PlatformTransactionManager.class);
+                entry.put("1", list);
+                json.put("entry", entry);
+                lists.add(json);
+            }
+        }
 
-			TransactionTemplate template = new TransactionTemplate(txManager);
+        String response = IWebService.webService(lists.toString(), "sms2hrpSendCargo");
+        JSONObject rps = JSONObject.parseObject(response);
+        if (rps == null || rps.equals("")) {
+            throw new BusinessLogicRunTimeException("网络错误!请稍后再试");
+        }
 
-			template.execute(new TransactionCallback<Object>() {
+        if (rps.get("code").equals("200")) {
+            PlatformTransactionManager txManager = Environ.getBean(PlatformTransactionManager.class);
 
-				@Override
-				public Object doInTransaction(TransactionStatus status) {
-					SendcargoMapper sendcargoMapper = sqlSession.getMapper(SendcargoMapper.class);
-					Sendcargo sendcargo = new Sendcargo();
-					for (int i = 0; i < split.length; i++) {
-						sendcargo.setId(split[i]);
-						sendcargo.setType(Byte.parseByte("1"));
-						sendcargoMapper.updateByPrimaryKeySelective(sendcargo);
-					}
-					return "success";
-				}
-			});
+            TransactionTemplate template = new TransactionTemplate(txManager);
 
-		}
-	}
+            template.execute(new TransactionCallback<Object>() {
+
+                @Override
+                public Object doInTransaction(TransactionStatus status) {
+                    SendcargoMapper sendcargoMapper = sqlSession.getMapper(SendcargoMapper.class);
+                    Sendcargo sendcargo = new Sendcargo();
+                    for (int i = 0; i < split.length; i++) {
+                        sendcargo.setId(split[i]);
+                        sendcargo.setType(Byte.parseByte("1"));
+                        sendcargoMapper.updateByPrimaryKeySelective(sendcargo);
+                    }
+                    return "success";
+                }
+            });
+
+        }
+    }
 }
