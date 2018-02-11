cef4edae208b659b12506293e6fb15f3fc8a5ebb
yadda <silenceisok@163.com>
2018-02-11 16:22:09 +0800

    webservice 增加日志

--- src/main/java/com/kingdee/eas/hrp/sms/service/impl/WebService.java
+++ src/main/java/com/kingdee/eas/hrp/sms/service/impl/WebService.java
@@ -6,6 +6,8 @@ import com.kingdee.eas.hrp.sms.util.SystemParamUtil;
 import com.kingdee.eas.hrp.sms.util.WSContext;
 import org.apache.axis.client.Call;
 import org.apache.axis.message.SOAPHeaderElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import javax.xml.namespace.QName;
@@ -14,9 +16,12 @@ import java.net.URL;
 @Service
 public class WebService extends BaseService implements IWebService {
 
+    private static final Logger logger = LoggerFactory.getLogger(WebService.class);
+
     @Override
     public String webService(String json, String name) {
         String response = "";
+        String sessionId = "";
         try {
             org.apache.axis.client.Service sv = new org.apache.axis.client.Service();
             Call call = (Call) sv.createCall();
@@ -24,18 +29,28 @@ public class WebService extends BaseService implements IWebService {
             call.setTargetEndpointAddress(new URL(SystemParamUtil.getString("sys", "hrp-sync-url")));
             call.setOperationName(new QName(SystemParamUtil.getString("sys", "hrp-sync-namespace"), name));
             call.setUseSOAPAction(true);
-            call.addParameter("json", org.apache.axis.encoding.XMLType.XSD_STRING, javax.xml.rpc.ParameterMode.IN);// 设置参数名,第二个参数表示String类型,第三个参数表示入参
+            // 设置参数名,第二个参数表示String类型,第三个参数表示入参
+            call.addParameter("json", org.apache.axis.encoding.XMLType.XSD_STRING, javax.xml.rpc.ParameterMode.IN);
             call.setReturnType(org.apache.axis.encoding.XMLType.XSD_STRING);
-            call.setTimeout(60000);// 设置超时时间
+            // 设置超时时间
+            call.setTimeout(60000);
             // 返回参数类型
             call.setReturnClass(String.class);
             // // 由于需要认证，需要设置sessionId
             SOAPHeaderElement soapHeaderElement = new SOAPHeaderElement(
                     SystemParamUtil.getString("sys", "hrp-sync-header-namespace"), "SessionId");
-            soapHeaderElement.setValue(loginInEAS());
+
+            sessionId = loginInEAS();
+
+            logger.debug(name + "==loginInEAS()获取到sessionId=" + sessionId);
+
+            soapHeaderElement.setValue(sessionId);
+
             call.addHeader(soapHeaderElement);
             response = (String) call.invoke(new Object[]{json});
         } catch (Exception e) {
+            logger.error("sessionId=" + sessionId);
+            logger.error(e.getMessage(), e);
             throw new BusinessLogicRunTimeException(e);
         }
         return response;
@@ -48,9 +63,12 @@ public class WebService extends BaseService implements IWebService {
             org.apache.axis.client.Service sv = new org.apache.axis.client.Service();
             Call call = (Call) sv.createCall();
             call.setUseSOAPAction(true);
-            call.setTargetEndpointAddress(new URL(SystemParamUtil.getString("sys", "hrp-login-url"))); // 设置要调用的接口地址以上一篇的为例子
-            call.setOperationName(new QName(SystemParamUtil.getString("sys", "hrp-login-namespace"), "login")); // 设置要调用的接口方法
-            call.addParameter("userName", org.apache.axis.encoding.XMLType.XSD_STRING, javax.xml.rpc.ParameterMode.IN);// 设置参数名,第二个参数表示String类型,第三个参数表示入参
+            // 设置要调用的接口地址以上一篇的为例子
+            call.setTargetEndpointAddress(new URL(SystemParamUtil.getString("sys", "hrp-login-url")));
+            // 设置要调用的接口方法
+            call.setOperationName(new QName(SystemParamUtil.getString("sys", "hrp-login-namespace"), "login"));
+            // 设置参数名,第二个参数表示String类型,第三个参数表示入参
+            call.addParameter("userName", org.apache.axis.encoding.XMLType.XSD_STRING, javax.xml.rpc.ParameterMode.IN);
             call.addParameter("password", org.apache.axis.encoding.XMLType.XSD_STRING, javax.xml.rpc.ParameterMode.IN);
             call.addParameter("slnName", org.apache.axis.encoding.XMLType.XSD_STRING, javax.xml.rpc.ParameterMode.IN);
             call.addParameter("dcName", org.apache.axis.encoding.XMLType.XSD_STRING, javax.xml.rpc.ParameterMode.IN);
@@ -58,9 +76,10 @@ public class WebService extends BaseService implements IWebService {
             call.addParameter("dbType", org.apache.axis.encoding.XMLType.XSD_INT, javax.xml.rpc.ParameterMode.IN);
             call.addParameter("authPattern", org.apache.axis.encoding.XMLType.XSD_STRING,
                     javax.xml.rpc.ParameterMode.IN);
+            // 超时时间为20s
+            call.setTimeout(20000);
 
-            call.setTimeout(20000);// 超时时间为20s
-            call.setReturnType(org.apache.axis.encoding.XMLType.XSD_ANYTYPE);//
+            call.setReturnType(org.apache.axis.encoding.XMLType.XSD_ANYTYPE);
             // 返回参数类型
             call.setReturnClass(WSContext.class);
             WSContext wsContext = (WSContext) call.invoke(new Object[]{
@@ -72,7 +91,8 @@ public class WebService extends BaseService implements IWebService {
                     SystemParamUtil.getInt("sys", "hrp-login-dbType"),
                     SystemParamUtil.getString("sys", "hrp-login-authPattern")});
 
-            System.out.println(wsContext);// 打印字符串
+            // 打印字符串
+            System.out.println(wsContext);
             sessionId = wsContext.getSessionId();
 
         } catch (Exception e) {
